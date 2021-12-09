bca01e87819affb27946bcdb2bfc592aaefe6400
jv <jeferson.veiiga@gmail.com>
2021-12-08 23:18:28 -0300

    new feature

---
+++ src/App.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container =  styled.div`
+    
+`;
+
+export const Header = styled.div`
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    background-color: darkblue;
+    height: 150px;
+    text-align: center;
+`;
+
+export const HeaderText = styled.div`
+    color: #FFF;
+    padding-top: 30px;
+    font-size: 40px;
+`;
+
+export  const Body = styled.div`
+    margin: auto;
+    max-width: 980px;
+    margin-bottom: 50px;
+`;
